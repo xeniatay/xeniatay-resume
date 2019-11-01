ea4eac68c116f19949fa30760f79e6d33812a87f
Xenia Tay <xeniatzy@gmail.com>
2019-11-01 00:40:14 -0400

    Update puppeteer

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -1,7 +1,8 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-	const browser = await puppeteer.launch();
+    // https://github.com/GoogleChrome/puppeteer/issues/3443
+    const browser = await puppeteer.launch({args: ['--no-sandbox']});
 	const page = await browser.newPage();
 	await page.goto("http://localhost:8080/"); // prod, xeniatay.com:8080
 	await page.emulateMedia("print");
